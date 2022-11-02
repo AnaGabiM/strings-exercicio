88ce840dc0f49e34e37e7d7a1f2b15ffe02b6292
AnaGabiM <ana.agma98@gmail.com>
2022-11-02 13:53:31 -0300

    Exercicio String Ozemela - Ana Gabriela

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-//Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres:\"BOAS VINDAS, mas não deixe o gato sair\""
+console.log(frase)
+
+let copia = (frase.replace(/verde/,"rosa").replace(/azul/,"laranja"))
+maiusculo = copia.substring(76,101).toUpperCase()
+resultado = copia.replace(/mas não deixe o gato sair/, maiusculo)
+
+verde = resultado.includes("verde")
+laranja = resultado.includes("laranja")
+
+console.log("contem verde?", verde)
+console.log("contem laranja?", laranja)
+
+console.log(resultado)
